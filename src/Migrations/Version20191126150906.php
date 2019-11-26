051d0a3fee5bf99a1fabd62a11622280a7bb263b
thomassouksouvanh <thomas.souksouvanh@sfr.fr>
2019-11-26 17:23:59 +0100

    category_id

--- src/Migrations/Version20191126150906.php
+++ src/Migrations/Version20191126150906.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191126140502 extends AbstractMigration
+final class Version20191126150906 extends AbstractMigration
 {
     public function getDescription() : string
     {
--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ Encore
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
 
-    /*
+    /*+
      * FEATURE CONFIG
      *
      * Enable & configure other features below. For a full
